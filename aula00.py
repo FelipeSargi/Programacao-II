a9c74924281effc5f7a2149958327ddbc78bf6c0
felipesargi <140860972+FelipeSargi@users.noreply.github.com>
2023-08-05 00:16:53 -0300

    atividade pronta

--- aula00.py
+++ aula00.py
@@ -1,1 +1,8 @@
-print ("ola")
+nome = "Felipe"
+sobrenome = "Sargi"
+idade = 18
+altura = 1.73
+peso = 80
+midade = (idade >= 18)
+
+print("\n",nome,"\n",sobrenome,"\n", idade,"anos","\n", altura, "m\n",peso , "Kg\n",  "Maior de idade ?\n",midade) 
