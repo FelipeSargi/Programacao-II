5ad24f1e835d504c2551f0c2f7eb11679b093734
felipesargi <140860972+FelipeSargi@users.noreply.github.com>
2023-08-05 00:28:19 -0300

    atividade prontav2

--- aula00.py
+++ aula00.py
@@ -5,4 +5,4 @@ altura = 1.73
 peso = 80
 midade = (idade >= 18)
 
-print("\n",nome , sobrenome,"\n", idade,"anos","\n", altura, "m\n",peso , "Kg\n",  "Maior de idade ?\n",midade) 
+print("\n",nome + sobrenome,"\n", idade,"anos","\n", altura, "m\n",peso , "Kg\n",  "Maior de idade ?\t.",midade , ".") 
