4ae6a8b17a2976daac46d47f41ffb4730529bb3e
felipesargi <140860972+FelipeSargi@users.noreply.github.com>
2023-08-12 00:55:41 -0300

    Comparação

--- atividade02.py
+++ atividade02.py
@@ -8,6 +8,7 @@ while True:
         altura = float(input("Entre com sua altura: "))
         break
     except ValueError:
+        os.system('cls')
         print("Valor inválido, tente novamente. ")
 
 while True:
@@ -15,17 +16,23 @@ while True:
         peso = float(input("Entre com seu peso: "))
         break
     except ValueError:
+        os.system('cls')
         print("Valor inválido, tente novamente. ")
-altura
+
 imc = float(peso/(altura*altura))
 
 if imc < 18.5:
+    os.system('cls')
     print("Sua classificação é: MAGREZA")
 elif imc >= 18.5 and imc <= 24.9:
+    os.system('cls')
     print("Sua classificação é: NORMAL")
 elif imc >= 25.0 and imc <= 29.9:
+    os.system('cls')
     print("Sua classificação é: SOBRE PESO")
 elif imc >= 30.0 and imc <= 39.9:
+    os.system('cls')
     print("Sua classificação é: OBESIDADE ")
 elif imc >= 40.0:
+    os.system('cls')
     print("Sua classificação é: OBESIDADE GRAVE ")
---
+++ atividade03.py
@@ -0,0 +1,25 @@
+import os 
+
+print("Programa le dois números e aponta qual deles é o maior. ")
+os.system('cls')
+while True:
+    try:
+        num1 = int(input("Insira o primeiro valor: "))
+        break
+    except ValueError:
+            os.system('cls')
+            print("\n\n\tDeu ruim, repete.")
+while True:
+    try:
+        num2 = int(input("Insira o segundo valor: "))
+        break
+    except ValueError:
+            os.system('cls')
+            print("\n\n\tDeu ruim, repete.")
+
+if num1 > num2:
+    print(num1,"é o maior")
+elif num1 < num2:
+    print(num2,"é o maior.")
+else:
+    print("ambos os valores são iguais. ")
