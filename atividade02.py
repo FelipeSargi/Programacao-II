404cb4592887d2f7bd04300819f44f11bf0e17e0
felipesargi <140860972+FelipeSargi@users.noreply.github.com>
2023-08-12 00:34:58 -0300

    Calculadora IMC

---
+++ atividade02.py
@@ -0,0 +1,31 @@
+import os
+
+print("\n\n\tPrograma que calcula seu Índice de Massa Corpórea - IMC.\n\n")
+os.system('cls')
+
+while True:
+    try:
+        altura = float(input("Entre com sua altura: "))
+        break
+    except ValueError:
+        print("Valor inválido, tente novamente. ")
+
+while True:
+    try:
+        peso = float(input("Entre com seu peso: "))
+        break
+    except ValueError:
+        print("Valor inválido, tente novamente. ")
+altura
+imc = float(peso/(altura*altura))
+
+if imc < 18.5:
+    print("Sua classificação é: MAGREZA")
+elif imc >= 18.5 and imc <= 24.9:
+    print("Sua classificação é: NORMAL")
+elif imc >= 25.0 and imc <= 29.9:
+    print("Sua classificação é: SOBRE PESO")
+elif imc >= 30.0 and imc <= 39.9:
+    print("Sua classificação é: OBESIDADE ")
+elif imc >= 40.0:
+    print("Sua classificação é: OBESIDADE GRAVE ")
