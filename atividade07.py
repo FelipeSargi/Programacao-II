99ce2517d06c008195e05b8ec682248a64c23b82
felipesargi <140860972+FelipeSargi@users.noreply.github.com>
2023-08-26 16:54:54 -0300

    matriz

--- atividade07.py
+++ atividade07.py
@@ -1,6 +1,8 @@
 import numpy as np
-
-dieimes_matriz = [[3,4,1], [3,2,1]]
-
-for x,y in dieimes_matriz:
-    print(x,y)
+dieimes_matriz = np.array([[3,4,1], 
+                           [3,2,1]])
+soma = 0
+for x in range(dieimes_matriz.shape[0]):
+    for y in range(dieimes_matriz.shape[1]):
+        soma = soma + dieimes_matriz[x][y]
+print(soma)
