1b5f76c17154aae6cbce9e0b5f8514bb94b8fc2b
felipesargi <140860972+FelipeSargi@users.noreply.github.com>
2023-11-03 21:33:34 -0300

    3.0

--- 2 bimestre/atividade03.py
+++ 2 bimestre/atividade03.py
@@ -1,39 +1,25 @@
 class Computador:
 
-    def __init__(self, marca, modelo, processador, memoria, hd):
-        
+    def __init__(self, marca, modelo, processador, memoriaram, hd):
         self.marca = marca
         self.modelo = modelo
         self.processador = processador
-        self.memoria = memoria
+        self.memoriaram = memoriaram
         self.hd = hd
-        
         self.monitor = None
     
- 
     def conectar_monitor(self, monitor):
-        
         if isinstance(monitor, Monitor):
-            
             self.monitor = monitor
-           
             return f"Monitor {monitor.marca} {monitor.modelo} está conectado ao computador {self.marca} {self.modelo}."
         else:
-           
             return "Monitor INVALIDO."
 
-
 class Monitor:
-  
     def __init__(self, marca, modelo):
-       
         self.marca = marca
         self.modelo = modelo
 
-computador1 = Computador("Chernobyll", "2.0", "Ryen 7 5000X", "64 GB", " 2TB")
-
-
-monitor1 = Monitor("Samsung", "UltraWide")
-
-
+computador1 = Computador("Chernobyll", "2.7", "Ryen 7 5000X", "64 GB", " 2TB")
+monitor1 = Monitor("Samsung", "UltraWides")
 print(computador1.conectar_monitor(monitor1))
